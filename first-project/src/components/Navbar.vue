5124be65661a0b31d5040e79bc335f3e66d12729
AlissonVieiraXavier <alissonvieiraxavier@hotmail.com>
2022-05-18 07:42:40 -0300

    a

---
+++ first-project/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Nav :navLinks="navLinks" :navConfig="navConfig" :btnConfig="btnConfig">
+      <img class="img" src="" alt="" srcset="" /> <!-- A slot For your project Logo -->
+    </Nav>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Nav from 'vue-nav-ui'
+export default defineComponent({
+    name:"NavBar",
+    components: {
+        Nav}
+
+})
+
+</script>
